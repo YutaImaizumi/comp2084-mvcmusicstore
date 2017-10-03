ceb06560c2b2d955e4f64d2b6c84238e2fa42ae8
Yuta Imaizumi <atamatock@gmail.com>
2017-10-03 11:09:31 -0400

    first commit

---
+++ MvcMusicStore-F2017/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using MvcMusicStore_F2017.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore_F2017.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            // create a new list in memory
+            var genres = new List<Genre>();
+
+            // create 10 pretend genre records
+            for (int i = 1; i <= 10; i++)
+            {
+                genres.Add(new Genre { Name = "Genre " + i.ToString() });
+            }
+
+            // give the list to the view with ViewBag
+            //ViewBag.genres = genres;
+
+            // pass the genre list as a parameter to the view
+            return View(genres);
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            // add the selected genre to the viewbag so we can display it in the browse view
+            ViewBag.genre = genre;
+            return View();
+        }
+
+    }
+}
